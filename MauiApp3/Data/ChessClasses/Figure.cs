1338040ff2fb4f361e0975bbfae051ef9fbd3868
A|R|R <64982730+Alikaponi09@users.noreply.github.com>
2023-02-16 20:15:46 +0300

    test

---
+++ MauiApp3/Data/ChessClasses/Figure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.Data.ChessClasses
+{
+    internal abstract class Figure
+    {
+        public abstract string Name { get; }
+
+        public string Pozition { get; set; }
+
+        public abstract void Move();
+    }
+}
---
+++ MauiApp3/Data/ChessClasses/Pawn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.Data.ChessClasses
+{
+    internal class Pawn : Figure
+    {
+        public override string Name { get; } = "P";
+
+        public override string Move() => "";
+    }
+}
