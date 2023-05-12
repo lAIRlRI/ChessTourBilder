1a6cf97e61e0a2a06c2bf0dbfc3d2f963f9d4d06
Alikaponi09 <renal_adamenko@mail.ru>
2023-05-12 21:54:05 +0400

    Api - Auto

---
+++ ChessTourBuilderApp/Data/Api/ApiControler.cs
@@ -0,0 +1,32 @@
+﻿using Azure;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTourBuilderApp.Data.Api
+{
+    internal class ApiControler
+    {
+        public static string baseURL = "https://localhost:7271/api/";
+
+        static HttpClient httpClient = new();
+
+        public static async Task<string> Post(string url, object body) 
+        {
+            var response = await httpClient.PostAsJsonAsync(baseURL+url, body);
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+
+        public static async Task<string> Get(string url)
+        {
+            var response = await httpClient.GetAsync(baseURL + url);
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+    }
+}
